39146f31fddeb4eab4d6671f1f65caed847992b8
Luis Cervantes <47375924+LuisCerv336@users.noreply.github.com>
2020-02-17 00:10:47 -0600

    CarpetasDelModelo
    Se agregaron las carpetas necesarias para manejar los datos dentro de un modelo parecido al vista controlador
    Además se han agregado las clases que interactuaran con los datos del empaque

---
+++ Daltonik_ERP/src/modelo/datos/ConnectURL.java
@@ -0,0 +1,49 @@
+package modelo.datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+
+public class ConnectURL {
+    public static Connection usu=null;
+    public ConnectURL() {
+    }
+
+    
+    public static Connection conectar(){
+        String url="jdbc:sqlserver://localhost:1433;databaseName=ERP2020";
+        try {
+            usu=DriverManager.getConnection(url,"sa","123");
+            System.out.println("Ya quedo carnal");
+        } catch (SQLException e) {
+            System.out.println("Conexion no establecida ... \n"+e.getMessage());
+        }
+        return usu;
+    }
+    public static void desconectar(){
+        try {
+            usu.close();
+        } catch (SQLException e) {
+            System.out.println("No existia una conexion a SQL que cerrar");
+        }
+    }
+    public ResultSet ejecutar(String sql){
+        Connection con= conectar();
+        Statement declara;
+        try {
+            declara=con.createStatement();
+            ResultSet respuesta=declara.executeQuery(sql);
+            desconectar();
+            return respuesta;
+            
+        } catch (SQLException e) {
+            System.out.println("Conexion no establecida ... \n"+e.getMessage());
+        }
+        return null;
+    }
+}
